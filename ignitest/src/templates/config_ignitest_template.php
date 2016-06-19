60f0eaf960b3d305378e4b37022ce1af38fc79da
Italo Paiva <italosensei@hotmail.com>
2016-06-19 03:09:30 -0300

    Adding database settings to ignitest config file template

--- ignitest/src/templates/config_ignitest_template.php
+++ ignitest/src/templates/config_ignitest_template.php
@@ -7,12 +7,43 @@
  	* Controllers Path
  	* Domains Path
 */
-define("APPPATH", "../../");
 
-// Controllers path
-$controllersPath = APPPATH."controllers/";
-define("CONTROLLERPATH", $controllersPath);
 
-// Domains path
-$domainsPath = APPPATH."data_types/";
-define("DOMAINPATH", $domainsPath);
+/*************** PATH SETTINGS *****************/
+
+	/****************************************************
+	 * This is the CodeIgniter /application folder path.
+	 * 
+	 * The default configuration of ignitest is placed inside 
+	 * application/tests as config_ignitest.php.
+	 *
+	 * If your config_ignitest.php file is not inside application/tests,
+	 * change it to your correct path.
+	*****************************************************/
+	define("APPPATH", "../../");
+
+	/**
+	 * Set here your controllers path
+	 */
+	$controllersPath = APPPATH."controllers/";
+	define("CONTROLLERPATH", $controllersPath);
+
+	/**
+	 * Set here your domains path
+	 */
+	$domainsPath = APPPATH."data_types/";
+	define("DOMAINPATH", $domainsPath);
+
+/***********************************************/
+
+
+/*************** DATABASE SETTINGS ***********************
+ * 
+ * Set here your database settings for integration tests
+ * 
+ * In the moment, Ignitest is only working with MySQL.
+ *********************************************************/
+	$HOST = "";
+	$USERNAME = "";
+	$PASSWORD = "";
+	$DATABASE_NAME = "";
